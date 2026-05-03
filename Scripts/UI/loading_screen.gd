bcbb0f0d9a8702943045b94a990fddb817e2b6d5
Jasmyhn <2245320195@qq.com>
2026-05-03 13:47:48 +0800

    异步加载

--- Scripts/UI/loading_screen.gd
+++ Scripts/UI/loading_screen.gd
@@ -3,12 +3,10 @@ extends Control
 const TARGET_SCENE_PATH := "res://Scenes/main_editor_screen.tscn"
 
 var _load_requested: bool = false
-var _cache_warmup_finished: bool = false
 var _scene_transition_started: bool = false
 
 func _ready() -> void:
 	_request_target_scene_load()
-	_warm_main_editor_caches()
 
 func _process(_delta: float) -> void:
 	_try_complete_transition()
@@ -22,19 +20,8 @@ func _request_target_scene_load() -> void:
 		return
 	_load_requested = true
 
-func _warm_main_editor_caches() -> void:
-	await get_tree().process_frame
-	FoodVisuals.build_food_texture_lookup()
-	await get_tree().process_frame
-	FoodVisuals.build_food_board_texture_lookup()
-	await get_tree().process_frame
-	LunchboxVisuals.build_role_base_texture_lookup()
-	await get_tree().process_frame
-	LunchboxVisuals.build_role_expansion_texture_lookup()
-	_cache_warmup_finished = true
-
 func _try_complete_transition() -> void:
-	if not _load_requested or not _cache_warmup_finished or _scene_transition_started:
+	if not _load_requested or _scene_transition_started:
 		return
 	var status: ResourceLoader.ThreadLoadStatus = ResourceLoader.load_threaded_get_status(TARGET_SCENE_PATH)
 	if status != ResourceLoader.THREAD_LOAD_LOADED:
